e51ea097110f870a6e73dc5518bcbc711117b8c8
Frank <fbelanger@ackroo.com>
2017-02-12 15:07:27 -0500

    UI fixups requires password, sign in (without register on btn)

--- app/src/main/java/com/example/frank/authexample_webapp_android/SessionsActivity.java
+++ app/src/main/java/com/example/frank/authexample_webapp_android/SessionsActivity.java
@@ -160,7 +160,11 @@ public class SessionsActivity extends AppCompatActivity implements LoaderCallbac
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
